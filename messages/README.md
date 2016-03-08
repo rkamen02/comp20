f879766ec22d5bd934a935bf69615ff45b2793e5
Ross Kamen <ross.kamen@tufts.edu>
2016-03-07 23:42:53 -0500

    finished lab6, everything working. adds lab6 readme.

---
+++ messages/README.md
@@ -0,0 +1,11 @@
+README.md
+
+1. All work is completed. Data is successfully received from data.json, but permission is denied from herokuapp (as it should be).
+
+2. I collaborated with Monika Dragulski.
+
+3. I spent 1 hour completing this assignment.
+
+Question:
+
+You can request from your local machine because you have the same origin as the file, but you can't request data from a different origin unless that origin uses CORS. 
--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,27 @@
-// Your JavaScript goes here...
+// parse function. grabs data from lab.js, parses data, then sends data to messages id
+function parse(){
+	var file2 = "https://messagehub.herokuapp.com/messages.json"
+	var file1 = "data.json"
+	var req = new XMLHttpRequest();
+	var data;
+	var result = "";
+
+	req.open("GET", file2, true);
+
+	req.onreadystatechange = function(){
+		if (req.readyState == 4){
+			data = JSON.parse(req.responseText);
+			for (var i = 0; i < data.length; i++){
+				result += data[i]["content"] + " " + data[i]["username"] + "<br>";
+			}
+
+			var messages = document.getElementById("messages");
+			messages.innerHTML = result;
+		}
+
+	}
+
+	req.send(null);
+
+
+}
